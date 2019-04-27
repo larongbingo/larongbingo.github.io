89fb892cf260b68b7d96b0498c480e0e6c4b8270
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 20:23:23 +0800

    feat(contact-form): send form handler

---
+++ send-message.js
@@ -0,0 +1,31 @@
+/**
+ * Handles the sending of messages to the message apis
+ */
+
+// Form Controls
+const titleTextBox = document.getElementById("title");
+const emailTextBox = document.getElementById("email");
+const messageTextArea = document.getElementById("message");
+const sendButton = document.getElementById("send");
+// End Form Controls
+
+// Constants
+const ENDPOINT = "http://contacts-webdev-project-contacts.1d35.starter-us-east-1.openshiftapps.com/message";
+// End Constants
+
+sendButton.addEventListener("click", (e) => {
+  // Since the button was in a form, it will try to send the contents as form-data
+  e.preventDefault();
+
+  const message = {
+    title: titleTextBox.value,
+    email: emailTextBox.value,
+    message: messageTextArea.value,
+  };
+
+  fetch(ENDPOINT, {
+    method: "POST",
+    body: JSON.stringify(message),
+  }).then(res => res.json())
+  .then((val) => console.log(val));
+});
